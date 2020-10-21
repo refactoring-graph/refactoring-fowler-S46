c3e483778b104f51069f27c3dedab73352a17df5
Henrique Brito <henriquejbrito@gmail.com>
2020-10-21 11:34:36 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -42,4 +42,10 @@ public class Movie {
 		}
 		return thisAmount;
 	}
+
+	public int getFrequentRenterPoints(int daysRented){
+		if ((getPriceCode() == Movie.NEW_RELEASE) &&
+				daysRented > 1) return 2;
+		else return 1;
+	}
 } 
